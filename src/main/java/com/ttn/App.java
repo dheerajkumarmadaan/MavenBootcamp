72680ea71d3f197a219b2711d1918f4e7b8fc248
Dheeraj Madaan <dheerajmadaangrp@gmail.com>
2022-10-01 14:21:01 +0530

    Initial change

---
+++ src/main/java/com/ttn/App.java
@@ -0,0 +1,7 @@
+package com.ttn;
+
+public class App {
+    public String returnSomething(){
+        return "hell1o";
+    }
+}
---
+++ src/test/java/com/ttn/AppTest.java
@@ -0,0 +1,14 @@
+package com.ttn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testData() {
+//        fail("not yet implemented");
+    }
+
+}
